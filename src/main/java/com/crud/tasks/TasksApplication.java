830e664479e06074f4ac96df132d379153055658
radoslaw-lazur <radoslaw.lazur@gmail.com>
2019-11-27 18:40:35 +0100

    22_2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -13,6 +13,7 @@ public class TasksApplication {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
+
 	//@Override
 	//protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
 		//return application.sources(TasksApplication.class);
