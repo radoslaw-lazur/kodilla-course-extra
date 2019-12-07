127ace6755b70db9a59b7a79107023afe7d51347
radoslaw-lazur <radoslaw.lazur@gmail.com>
2019-12-07 19:31:19 +0100

    23.3

---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,41 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleEmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+    private static  final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+
+    public void send(final Mail mail) {
+        LOGGER.info("Starting e-mail preparation...");
+        try {
+            SimpleMailMessage mailMessage = createMailMessege(mail);
+            javaMailSender.send(mailMessage);
+            LOGGER.info("E-mail has been sent");
+        } catch (MailException e) {
+            LOGGER.info("Failed to process e-mail sending: ", e.getMessage(), e);
+        }
+    }
+
+    public SimpleMailMessage createMailMessege(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessege());
+        if((mail.getToCc() == null) || (mail.getToCc().equals(""))) {
+            mailMessage.setCc();
+        } else {
+            mailMessage.setCc(mail.getToCc());
+        }
+        return mailMessage;
+    }
+}
---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail() {
+        //Given
+        Mail mail = new Mail("test1@test.com","null", "Test", "Test Messege");
+        //When
+        simpleEmailService.send(mail);
+        //Then
+        Mockito.verify(javaMailSender, Mockito.times(1)).
+                send(simpleEmailService.createMailMessege(mail));
+    }
+}
