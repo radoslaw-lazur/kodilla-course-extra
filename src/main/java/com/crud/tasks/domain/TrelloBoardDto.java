d79173d33664653d57ebbc6705b9877d5e1730f2
radoslaw-lazur <radoslaw.lazur@gmail.com>
2020-02-19 19:09:41 +0100

    commit

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
     @JsonProperty("id")
